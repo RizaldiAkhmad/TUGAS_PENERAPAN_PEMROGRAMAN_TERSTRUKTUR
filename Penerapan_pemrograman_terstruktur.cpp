36627d13fb47bd3e85a0ebe8d7b4fd2c05fb8d3b
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-12-31 14:48:10 +0800

    Menampilkan Nilai Hasil Akhir

--- Penerapan_pemrograman_terstruktur.cpp
+++ Penerapan_pemrograman_terstruktur.cpp
@@ -103,6 +103,12 @@ int main(){
 			r++;
 		}while(r<5);cout << endl;
 		
+			cout << endl << endl;
+	cout << "NILAI AKHIR:   " << endl;
+	cout <<"Nilai A1   :   "<<ha[0] << endl;
+	cout <<"Nilai A2   :   "<<ha[1] << endl;
+	cout <<"Nilai A3   :   "<<ha[2] << endl;
+		
 }
 
 
